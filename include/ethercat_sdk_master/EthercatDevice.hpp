4d6c8b46eb5d54d6a79c4fda390bc5a12a7a4fbd
jungerj <jungerj.engineer@gmail.com>
2021-02-06 21:33:54 +0100

    removed SDO template hell

--- include/ethercat_sdk_master/EthercatDevice.hpp
+++ include/ethercat_sdk_master/EthercatDevice.hpp
@@ -20,6 +20,8 @@
 #include <string>
 #include <cstdint>
 #include <memory>
+#include <thread>
+#include <chrono>
 namespace ecat_master {
 
 /*!
@@ -77,30 +79,6 @@ public:
 public:
 
   /*!
-   * Send a write SDO of type Value to the device.
-   * @note Using SDO and PDO communication simultaneously is usually a bad idea.
-   * @param[in] index index of the SDO (16 bit).
-   * @param[in] subindex sub index of the SDO (8 bit).
-   * @param[in] completeAcces]ns true if all subindices are overwritten.
-   * @param[in] value Value to be written.
-   * @return true if successful.
-   */
-  template <typename Value>
-  bool sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, Value value);
-
-  /*!
-   * Send a read SDO of type Value to the device.
-   * @note Using SDO and PDO communication simultaneously is usually a bad idea.
-   * @param[in] index index of the SDO (16 bit).
-   * @param[in] subindex sub index of the SDO (8 bit).
-   * @param[in] completeAcces]ns true if all subindices are read.
-   * @param[out] value Value which is read from the device.
-   * @return true if successful.
-   */
-  template <typename Value>
-  bool sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, Value& value);
-
-  /*!
    * Send a write SDO of type Value to the device and confirm by reading
    * the same value afterwards.
    * @note Using SDO and PDO communication simultaneously is usually a bad idea.
@@ -112,79 +90,18 @@ public:
    * @return true if the read value corresponds to the written value.
    */
   template <typename Value>
-  bool sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, Value value, float delay = 0);
+  bool sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, Value value, float delay = 0) {
+    Value testVal;
+    bool success = true;
+    success &= sendSdoWrite(index, subindex, completeAccess, value);
+    std::this_thread::sleep_for(std::chrono::microseconds(static_cast<unsigned int>(delay)));
+    success &= sendSdoRead(index, subindex, completeAccess, testVal);
+    return (success & (value == testVal));
+  }
 
 protected:
   std::string name_;
   double timeStep_{0.0};
 };
-
-// template specialization sdo writing
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const int8_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const int16_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const int32_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const int64_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const uint8_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const uint16_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const uint32_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const uint64_t value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const float value);
-template <>
-bool EthercatDevice::sendSdoWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, const double value);
-
-// template specialization sdo reading
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, int8_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, int16_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, int32_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, int64_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint8_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint16_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint32_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint64_t& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, float& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, double& value);
-template <>
-bool EthercatDevice::sendSdoRead(const uint16_t index, const uint8_t subindex, const bool completeAccess, std::string& value);
-
-// template specialization sdo verivy write
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, int8_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, int16_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, int32_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, int64_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint8_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint16_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint32_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, uint64_t value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, float value, float delay);
-template <>
-bool EthercatDevice::sdoVerifyWrite(const uint16_t index, const uint8_t subindex, const bool completeAccess, double value, float delay);
 
 } // namespace ecat_master
