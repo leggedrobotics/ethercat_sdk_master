06ea86b4bf648d006de1d4d77616e23739fe4617
Lennart Nachtigall <firesurfer@firesurfer.de>
2023-12-16 12:54:04 +0100

    singleton will now spin each master asynchronously

--- ethercat_sdk_master/include/ethercat_sdk_master/EthercatMasterSingleton.hpp
+++ ethercat_sdk_master/include/ethercat_sdk_master/EthercatMasterSingleton.hpp
@@ -17,10 +17,14 @@ namespace ecat_master {
 
                 if(ecat_masters_.find(config.networkInterface) == ecat_masters_.end())
                 {
+                    MELO_INFO_STREAM("Setting up new EthercatMaster on interface: " << config.networkInterface << " and updating it");
                     auto master = std::make_shared<EthercatMaster>();
                     master->loadEthercatMasterConfiguration(config);
+                    
+                    //Spin the master asynchronously
+                    spin_threads_.emplace(config.networkInterface, std::make_unique<std::thread>(std::bind(&EthercatMasterSingleton::spin,this, std::placeholders::_1), master));
                 }
-
+                
                 if(config != ecat_masters_[config.networkInterface]->getConfiguration()){
                     //Print warning or abort if the configuration does not match!
                     MELO_WARN_STREAM("Ethercat master configurations do not match for bus: " << config.networkInterface);
@@ -44,9 +48,27 @@ namespace ecat_master {
         EthercatMasterSingleton(){
 
         }
+        ~EthercatMasterSingleton() {
+            abort_ = true;
+
+            for(const auto& [interface, thread]: spin_threads_){
+                thread->join();
+            }
+            for(const auto &  [interfrace, master]: ecat_masters_){
+                master->preShutdown();
+                master->shutdown();
+            }
+        }
+        void spin(std::shared_ptr<EthercatMaster> master_){
+            while(!abort_){
+                master_->update(UpdateMode::StandaloneEnforceRate);
+            }
+        }
 
         std::map<std::string,std::shared_ptr<EthercatMaster>> ecat_masters_;
-        static std::recursive_mutex lock_;
+        std::map<std::string, std::unique_ptr<std::thread>> spin_threads_;
+        std::recursive_mutex lock_;
+        std::atomic_bool abort_ = false;
     };
 
 }
